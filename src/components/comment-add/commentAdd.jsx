917a8b2fd47c382b5bdd516ca110e73d88196e56
shenmingfeng <1191840939@qq.com>
2020-12-09 17:36:19 +0800

    2

---
+++ src/components/comment-add/commentAdd.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class commentAdd extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default commentAdd;
